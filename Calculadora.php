3a2ebfab3c0c3a388e012628f1d7211f656a9b9c
C-jimenez21 <cristianjjc21@gmail.com>
2023-06-05 16:46:42 -0500

    style: :lipstick: Creacion del diseño base de la calculadora
    Se asginaron estilos para cada elemento que compone la calculadora, ademas de dividir por grupo las diferentes configuraciones

--- Calculadora.php
+++ Calculadora.php
@@ -4,21 +4,86 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        *{
+            margin: 0px;
+            padding:0px;
+            border-sizing: border-box;
+        }
+        .container{
+            margin:auto;
+            padding-block:20px;
+            background-color:black;
+            border:2px solid white;
+            width: 400px;
+            height: 500px;
+            border-radius: 50px;
+            box-shadow: 10px 10px 20px;
+            display:grid;
+            grid-row: 1fr 1fr 1fr 1fr 1fr;
+            justify-content:center;
+            align-items:center;
+        }
+        .textOut{
+            background-color:black;
+            border-radius: 20px;
+            padding:20px;
+            font-size:1.3em;
+            width:89%;
+            color:white;
+        }
+        .numBtn{
+            padding: 30px 35px;
+            border-radius: 100vh;
+            cursor: pointer;
+            background-color:lightgray;
+            
+        }
+        .opBtn{
+            padding: 30px 35px;
+            border-radius:100vh;
+            background-color:orange;
+            cursor: pointer;
+        }
+        .btnRe{
+            background-color:green;
+        }
+        .btnCC{
+            background-color:red;
+            
+        }
+    </style>
     <title>Calculadora</title>
 </head>
 <body>
-    <h1>Calculadora basica</h1>
     <form action="Calculadora.php" method="POST">
-        <input type="number" name="valor1" placeholder="Ingrese un número"><br>
-        <input type="number" name="valor2" placeholder="Ingrese un número"><br>
-        <input type="radio" name="operacion" value="+"><label>Sumar</label>
-        <input type="radio" name="operacion" value="-"><label>Restar</label>
-        <input type="radio" name="operacion" value="*"><label>Multiplicar</label>
-        <input type="radio" name="operacion" value="/"><label>Dividir</label>
-        <br>
-        <input type="submit" value="Enviar"><br><br>
-
-
+        <div class="container">
+        <input type="text" class="textOut" name="valor"><br>
+        <div class="group">
+            <input type="submit" class ="numBtn" name="Numero" value="7">
+            <input type="submit" class ="numBtn" name="Numero" value="8">
+            <input type="submit" class ="numBtn" name="Numero" value="9"> 
+            <input type="submit" class ="opBtn" name="Operacion" value="+">
+        </div>
+        <div class="group">
+            <input type="submit" class ="numBtn" name="Numero" value="4">
+            <input type="submit" class ="numBtn" name="Numero" value="5">
+            <input type="submit" class ="numBtn" name="Numero" value="6">
+            <input type="submit" class ="opBtn" name="Operacion" value="-">
+        </div>
+        <div class="group">
+            <input type="submit" class ="numBtn" name="Numero" value="1">
+            <input type="submit" class ="numBtn" name="Numero" value="2">
+            <input type="submit" class ="numBtn" name="Numero" value="3">
+            <input type="submit" class ="opBtn" name="Operacion" value="*">
+        </div>
+        <div class="group">
+            <input type="submit" class ="numBtn btnCC" name="Clear" value="cc">
+            <input type="submit" class ="numBtn" name="Numero" value="0">
+            <input type="submit" class ="numBtn btnRe" name="Resultado" value="=">
+            <input type="submit" class ="opBtn" name="Operacion" value="/">
+        </div>
+        </div>
         <?php
     //header("Content-type: application/json");
     print_r($_POST);
